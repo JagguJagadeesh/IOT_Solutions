41479aae2c09d6bc3db0d142f0193d476e901346
JagguJagadeesh <jagadeesh121502@gmail.com>
2024-07-22 20:41:25 +0530

    Initial Commit

---
+++ IOT_Solutions/src/main/java/com/Jagadeesh/IOT_Solutions/IotSolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.Jagadeesh.IOT_Solutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotSolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotSolutionsApplication.class, args);
+	}
+
+}
---
+++ IOT_Solutions/src/test/java/com/Jagadeesh/IOT_Solutions/IotSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Jagadeesh.IOT_Solutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotSolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
